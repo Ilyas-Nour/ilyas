19481cfc67a9bf14be50aee171ea30c06e13695c
Mr-Ilyas-Nour <187109104+Ilyas-Nour@users.noreply.github.com>
2026-03-18 02:52:00 +0000

    style: final responsive polish for About section (optimized text sizes and vertical distribution)

--- dist/assets/three-vendor-Cf5iJ7nS.js
+++ dist/assets/three-vendor-Cf5iJ7nS.js
@@ -1,4 +1,4 @@
-import{a as hn,j as Ei,R as YM,b as ZM}from"./react-vendor-70Kze2V4.js";import{aa as $M,ab as jM,ac as JM,ad as KM,ae as QM,af as O_,ag as e1}from"./vendor-DPzc8ZBC.js";/**
+import{a as hn,j as Ei,R as YM,b as ZM}from"./react-vendor-3e8RV81r.js";import{a9 as $M,aa as jM,ab as JM,ac as KM,ad as QM,ae as O_,af as e1}from"./vendor-NkOT0WKn.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
